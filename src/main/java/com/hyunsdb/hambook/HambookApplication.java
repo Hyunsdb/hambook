d0d2b52a78293ec590372413ede6679851e990d6
hyuna <hyunsdb@naver.com>
2022-01-05 23:27:18 +0900

    Initial commit

---
+++ src/main/java/com/hyunsdb/hambook/HambookApplication.java
@@ -0,0 +1,13 @@
+package com.hyunsdb.hambook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HambookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HambookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyunsdb/hambook/HambookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunsdb.hambook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HambookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
