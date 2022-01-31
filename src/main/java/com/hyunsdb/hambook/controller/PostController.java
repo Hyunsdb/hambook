98f7506f9f138ae67a02d27d0fc732a2d0cfbfdb
hyuna <hyunsdb@naver.com>
2022-01-31 18:45:40 +0900

    feature: writer-add post
    작가 게시판 게시글 등록

--- src/main/java/com/hyunsdb/hambook/controller/PostController.java
+++ src/main/java/com/hyunsdb/hambook/controller/PostController.java
@@ -1,10 +1,13 @@
 package com.hyunsdb.hambook.controller;
 
+import com.hyunsdb.hambook.dto.PostFormDto;
 import com.hyunsdb.hambook.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,9 +17,21 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/list")
-    public String boardList(Model model){
+    public String boardList(Model model) {
         model.addAttribute("posts", postService.getPostList());
         return "board/boardList";
     }
 
+    @GetMapping("/post/add")
+    public String postAddForm(Model model) {
+        model.addAttribute("postFormDto", new PostFormDto());
+        return "board/boardForm";
+    }
+
+    @PostMapping("/post/add")
+    public String postAdd(@ModelAttribute PostFormDto postFormDto) {
+        postService.addPost(postFormDto);
+
+        return "redirect:/";
+    }
 }
--- src/main/java/com/hyunsdb/hambook/service/PostService.java
+++ src/main/java/com/hyunsdb/hambook/service/PostService.java
@@ -1,8 +1,10 @@
 package com.hyunsdb.hambook.service;
 
+import com.hyunsdb.hambook.dto.PostFormDto;
 import com.hyunsdb.hambook.entity.Post;
 import com.hyunsdb.hambook.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +14,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
 
     public List<Post> getPostList(){
         return postRepository.findAll();
     }
+
+    public Long addPost(PostFormDto postFormDto) {
+        return postRepository.save(modelMapper.map(postFormDto, Post.class)).getPid();
+    }
+
 }
