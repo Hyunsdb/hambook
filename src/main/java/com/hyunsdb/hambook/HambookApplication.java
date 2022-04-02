1bba6ddec3b06f6f40a1068bac5cb82269890176
Hyunsdb <40584381+Hyunsdb@users.noreply.github.com>
2022-04-02 22:54:02 +0900

    Revert "Feature/writer"

--- src/main/java/com/hyunsdb/hambook/HambookApplication.java
+++ src/main/java/com/hyunsdb/hambook/HambookApplication.java
@@ -2,10 +2,8 @@ package com.hyunsdb.hambook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class HambookApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hyunsdb/hambook/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hyunsdb.hambook.repository;
-
-import com.hyunsdb.hambook.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface PostRepository extends JpaRepository<Post,Long>, QuerydslPredicateExecutor<Post>, PostSearchRepository {
-}
